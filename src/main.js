b22f58fbd8d854bb6903c4a79a9aed5a520ec817
jhzeng <jhzeng04@gmail.com>
2025-09-01 17:05:46 +0800

    return

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 // swiper
 import "swiper/css";
-// Meting 播放器
-import Meting from "@meting/aplayer-vue";
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -16,27 +14,6 @@ pinia.use(piniaPluginPersistedstate);
 app.use(pinia);
 app.mount("#app");
 
-// ----------------------------
-// 浏览器挂载 Meting 播放器
-// ----------------------------
-if (typeof window !== "undefined") {
-  window.addEventListener("load", () => {
-    const container = document.createElement("div");
-    container.id = "player-container";
-    document.body.appendChild(container);
-
-    createApp({
-      render: () =>
-        h(Meting, {
-          server: import.meta.env.VITE_SONG_SERVER,
-          type: import.meta.env.VITE_SONG_TYPE,
-          id: import.meta.env.VITE_SONG_ID,
-          api: import.meta.env.VITE_SONG_API,
-          style: "position: fixed; bottom: 0; left: 0; width: 100%; z-index: 9999;",
-        }),
-    }).mount("#player-container");
-  });
-}
 
 // PWA
 navigator.serviceWorker.addEventListener("controllerchange", () => {
